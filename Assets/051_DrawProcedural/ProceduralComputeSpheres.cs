a54eb6f41f9601ef19c77590b9dd70fd916242d2
Ronja <ronja@boehringer-mueller.de>
2020-09-16 16:53:28 +0200

    Graphics.DrawProcedural tutorial

---
+++ Assets/051_DrawProcedural/ProceduralComputeSpheres.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ProceduralComputeSpheres : MonoBehaviour
+{
+    //rough outline for data
+    public int SphereAmount = 17;
+    public ComputeShader Shader;
+
+    //what is rendered
+    public Mesh Mesh;
+    public Material Material;
+    public float Scale = 1;
+    
+    //internal data
+    ComputeBuffer resultBuffer;
+    ComputeBuffer meshTriangles;
+    ComputeBuffer meshPositions;
+    int kernel;
+    uint threadGroupSize;
+    Bounds bounds;
+    int threadGroups;
+
+    void Start()
+    {
+        //program we're executing
+        kernel = Shader.FindKernel("Spheres");
+        Shader.GetKernelThreadGroupSizes(kernel, out threadGroupSize, out _, out _);
+        
+        //amount of thread groups we'll need to dispatch
+        threadGroups = (int) ((SphereAmount + (threadGroupSize - 1)) / threadGroupSize);
+        
+        //gpu buffer for the sphere positions
+        resultBuffer = new ComputeBuffer(SphereAmount, sizeof(float) * 3);
+        
+        //gpu buffers for the mesh
+        int[] triangles = Mesh.triangles;
+        meshTriangles = new ComputeBuffer(triangles.Length, sizeof(int));
+        meshTriangles.SetData(triangles);
+        Vector3[] positions = Mesh.vertices.Select(p => p * Scale).ToArray(); //adjust scale here
+        meshPositions = new ComputeBuffer(positions.Length, sizeof(float) * 3);
+        meshPositions.SetData(positions);
+
+        //give data to shaders
+        Shader.SetBuffer(kernel, "Result", resultBuffer);
+        
+        Material.SetBuffer("SphereLocations", resultBuffer);
+        Material.SetBuffer("Triangles", meshTriangles);
+        Material.SetBuffer("Positions", meshPositions);
+        
+        //bounds for frustum culling (20 is a magic number (radius) from the compute shader)
+        bounds = new Bounds(Vector3.zero, Vector3.one * 20);
+    }
+
+    void Update()
+    {
+        //calculate positions
+        Shader.SetFloat("Time", Time.time);
+        Shader.Dispatch(kernel, threadGroups, 1, 1);
+        
+        //draw result
+        Graphics.DrawProcedural(Material, bounds, MeshTopology.Triangles, meshTriangles.count, SphereAmount);
+    }
+
+    void OnDestroy()
+    {
+        resultBuffer.Dispose();
+        meshTriangles.Dispose();
+        meshPositions.Dispose();
+    }
+}
---
+++ Assets/051_DrawProcedural/ProceduralSpheres.shader
@@ -0,0 +1,50 @@
+﻿Shader "Tutorial/051_ProceduralSpheres"{
+	//show values to edit in inspector
+	Properties{
+		[HDR] _Color ("Tint", Color) = (0, 0, 0, 1)
+	}
+
+	SubShader{
+		//the material is completely non-transparent and is rendered at the same time as the other opaque geometry
+		Tags{ "RenderType"="Opaque" "Queue"="Geometry" }
+
+		Pass{
+			CGPROGRAM
+
+			//include useful shader functions
+			#include "UnityCG.cginc"
+
+			//define vertex and fragment shader functions
+			#pragma vertex vert
+			#pragma fragment frag
+
+			//tint of the texture
+			fixed4 _Color;
+
+			//buffers
+			StructuredBuffer<float3> SphereLocations;
+			StructuredBuffer<int> Triangles;
+			StructuredBuffer<float3> Positions;
+
+			//the vertex shader function
+			float4 vert(uint vertex_id: SV_VertexID, uint instance_id: SV_InstanceID) : SV_POSITION{
+				//get vertex position
+				int positionIndex = Triangles[vertex_id];
+				float3 position = Positions[positionIndex];
+				//add sphere position
+				position += SphereLocations[instance_id];
+				//convert the vertex position from world space to clip space
+				return mul(UNITY_MATRIX_VP, float4(position, 1));
+			}
+
+			//the fragment shader function
+			fixed4 frag() : SV_TARGET{
+				//return the final color to be drawn on screen
+				return _Color;
+			}
+			
+			ENDCG
+		}
+	}
+	Fallback "VertexLit"
+}
